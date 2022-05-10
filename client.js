64549086e2837f9329e3cb8d1b62502d548985fd
Courtney Azar <courtney.a.azar@gmail.com>
2022-05-10 16:47:15 -0500

    live code with Edan, my code is commented out

--- client.js
+++ client.js
@@ -39,38 +39,89 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-function calculateTotalBonus(employees) {
-  let i = 0;
-  bonusPercentage = 0;
-  let calculateTotalBonus = (employees[i].annualSalary / bonusPercentage);
-  for (let employee of employees){
-    if (employees.reviewRating >= 2){
-      bonusPercentage += 0;
-    }
-    else if(employees.reviewRating == 3){
-      bonusPercentage += 4;
-    }
-    else if(employees.reviewRating == 4){
-      bonusPercentage += 6;
-    }
-    else if(employees.reviewRating == 5){
-      bonusPercentage += 10;
-  }
-function addNewEmployee (name, bonusPercentage, totalCompensation, totalBonus) {
+
+//----------------------My code here:----------------------- Nothing is working
+// function calculateTotalBonus(employees) {
+//   let i = 0;
+//   bonusPercentage = 0;
+//   let calculateTotalBonus = (employees[i].annualSalary / bonusPercentage);
+//   for (let employee of employees){
+//     if (employees.reviewRating >= 2){
+//       bonusPercentage += 0;
+//     }
+//     else if(employees.reviewRating == 3){
+//       bonusPercentage += 4;
+//     }
+//     else if(employees.reviewRating == 4){
+//       bonusPercentage += 6;
+//     }
+//     else if(employees.reviewRating == 5){
+//       bonusPercentage += 10;
+//   }
+// function addNewEmployee (name, bonusPercentage, totalCompensation, totalBonus) {
   
-  let newEmployee = {
-    name: name,
+//   let newEmployee = {
+//     name: name,
+//     bonusPercentage: bonusPercentage,
+//     totalCompensation: totalCompensation,
+//     totalBonus: totalBonus 
+//   }
+//   newEmployee.push(employees);
+// return newEmployee;
+// }
+// return calculateTotalBonus; 
+// }
+// }
+// calculateTotalBonus(employees);
+// console.log(employees[0].annualSalary);
+
+// console.log( employees );
+
+// ------------------Edan's Code here:-----------------------
+
+console.log(employees);
+
+for (let employee of employees) {
+  let returnValue = calculateTotalBonus(employee);
+  console.log(returnValue);
+}
+
+function calculateTotalBonus(employee){
+  console.log('in calculate total bonus', employee);
+  let bonusPercentage;
+
+  if(employee.reviewRating <= 2){
+    bonusPercentage = 0;
+  }
+  else if (employee.reviewRating === 3) {
+    bonusPercentage = 0.04;
+  }
+  else if (employee.reviewRating === 4) {
+    bonusPercentage = 0.06;
+  }
+  else if (employee.reviewRating === 5) {
+    bonusPercentage = 0.10;
+  }
+
+  if (employee.employeeNumber.length === 4) {
+    bonusPercentage += 0.05;
+  }
+  if (employee.annualSalary > 65000) {
+    bonusPercentage -= 0.01;
+  }
+  if (bonusPercentage > 0.13) {
+    bonusPercentage = 0.13;
+  }
+  if (bonusPercentage < 0){
+    bonusPercentage = 0;
+ }
+ let totalBonus = employee.annualSalary * bonusPercentage;
+ let totalCompensation = Number(employee.annualSalary) + totalBonus;
+
+  return {
+    name: employee.name,
     bonusPercentage: bonusPercentage,
     totalCompensation: totalCompensation,
-    totalBonus: totalBonus 
-  }
-  newEmployee.push(employees);
-return newEmployee;
+    totalBonus: totalBonus
+  };
 }
-return calculateTotalBonus; 
-}
-}
-calculateTotalBonus(employees);
-console.log(employees[0].annualSalary);
-
-console.log( employees );
